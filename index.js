663247cc649f5fa181623b1f2ce4d803a422233c
mikethompson100 <mikethompson100@hotmail.com>
2025-06-21 16:55:52 -0400

    Ran a test that opens a link in the browser.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 import ExcelJS from 'exceljs';
+import { exec } from 'child_process';
 
 const workbook = new ExcelJS.Workbook();
 
@@ -7,6 +8,7 @@ async function main() {
   await workbook.xlsx.readFile("./cli-data.xlsx");
   const worksheet = workbook.getWorksheet('Sheet1');
   const nameCol = worksheet.getColumn('B');
-  console.log(nameCol.values[7]);
+  console.log(nameCol.values[8].hyperlink);  
+  exec(`open -a "Google Chrome" "${nameCol.values[8].hyperlink}"`); // macOS
 }
 main();
